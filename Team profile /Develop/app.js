dbb75f2a0f534f0410cd29621e2c137ad11fc2a3
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-08-27 13:57:27 -0400

    added app.js async

--- Team profile /Develop/app.js
+++ Team profile /Develop/app.js
@@ -10,6 +10,101 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const wholeTeam = [
+    {
+        type: "input",
+        message: "What is the team members name?",
+        name: "name"
+    },
+
+    {
+        type: "input",
+        message: "What is the team members id?",
+        name: "id"
+    },
+
+    {
+        type: "input",
+        message: "What is the team members email?",
+        name: "email"
+    },
+]
+
+const manager = [
+    {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNum",
+    },
+]
+
+const engineerQ = [
+    {
+        type: "input",
+        message: "What is the team members github name?",
+        name: "githubName"
+    }
+]
+
+const internQ = [
+    {
+        type: "input",
+        message: "What is the intern's school name?",
+        name: "school"
+    }
+]
+
+const roleQ = [
+    {
+        type: "list",
+        message: "What type of team member would you like to add?",
+        name: "teamMember",
+        choices: ["Engineer", "Intern", "I dont want to add another employee!"]
+    }
+
+]
+
+let employees = []
+let person;
+
+async function init() {
+    console.log ("Manager details:");
+    const allMemberAs = await inquirer.prompt(wholeTeam);
+    const managerAs = await inquirer.prompt(manager);
+    person = new Manager(allMemberAs.name, allMemberAs.id, allMemberAs.email, managerAs.officeNum);
+    employees.push(person);
+    console.log(employees)
+    otherMembers();
+}
+init ();
+
+
+async function otherMembers() {
+    const chooseMem = await inquirer.prompt(roleQ);
+    if (chooseMem.teamMember === "Engineer") {
+        const allEngineerA  = await inquirer.prompt(engineerQ);
+        const generalEngQs = await inquirer.prompt(wholeTeam);
+        person = new Engineer(generalEngQs.name, generalEngQs.id, generalEngQs.email, allEngineerA.githubName)
+        employees.push(person);
+        otherMembers();
+    } else if (chooseMem.teamMember === "Intern") {
+        const allInternA  = await inquirer.prompt(internQ);
+        const generalInQs = await inquirer.prompt(wholeTeam);
+        person = new Intern(generalInQs.name, generalInQs.id, generalInQs.email, allInternA.school)
+        employees.push(person);
+        otherMembers();
+
+    } else if (chooseMem.teamMember ==="I dont want to add another employee!") {
+        fs.writeFile(outputPath, render(employees), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Congrats, the file is saved!")
+        })
+        return;
+    }
+
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
