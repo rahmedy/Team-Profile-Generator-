c5e132fd6f97bc44f0e8d84e5210c23217309bdc
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-08-28 15:03:57 -0400

    Manager.js contnet

--- Team profile /Develop/lib/Manager.js
+++ Team profile /Develop/lib/Manager.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("../lib/Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNum) {
+        super (name, id, email);
+        this.officeNumber = officeNum;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
+
+module.exports = Manager;
+
