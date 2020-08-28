6265c45eb586b1bcb939b936fc32436fa39de7cd
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-08-28 15:46:30 -0400

    tweaked app.js

--- Team profile /Develop/app.js
+++ Team profile /Develop/app.js
@@ -1,109 +1,109 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 
 const wholeTeam = [
-    {
-        type: "input",
-        message: "What is the team members name?",
-        name: "name"
-    },
+	{
+		type: 'input',
+		message: 'What is the team members name?',
+		name: 'name'
+	},
 
-    {
-        type: "input",
-        message: "What is the team members id?",
-        name: "id"
-    },
+	{
+		type: 'input',
+		message: 'What is the team members id?',
+		name: 'id'
+	},
 
-    {
-        type: "input",
-        message: "What is the team members email?",
-        name: "email"
-    },
-]
+	{
+		type: 'input',
+		message: 'What is the team members email?',
+		name: 'email'
+	}
+];
 
 const manager = [
-    {
-        type: "input",
-        message: "What is your office number?",
-        name: "officeNum",
-    },
-]
+	{
+		type: 'input',
+		message: 'What is your office number?',
+		name: 'officeNum'
+	}
+];
 
 const engineerQ = [
-    {
-        type: "input",
-        message: "What is the team members github name?",
-        name: "githubName"
-    }
-]
+	{
+		type: 'input',
+		message: 'What is the team members github name?',
+		name: 'githubName'
+	}
+];
 
 const intern = [
-    {
-        type: "input",
-        message: "What is the intern's school name?",
-        name: "school"
-    }
-]
+	{
+		type: 'input',
+		message: "What is the intern's school name?",
+		name: 'school'
+	}
+];
 
 const role = [
-    {
-        type: "list",
-        message: "What type of team member would you like to add?",
-        name: "teamMember",
-        choices: ["Engineer", "Intern", "I dont want to add another employee!"]
-    }
+	{
+		type: 'list',
+		message: 'What type of team member would you like to add?',
+		name: 'teamMember',
+		choices: [ 'Engineer', 'Intern', 'I dont want to add another employee!' ]
+	}
+];
 
-]
+// variables that will be changed
 
-let employees = []
+let employees = [];
 let person;
 
 async function init() {
-    console.log ("Manager details:");
-    const allMemberAs = await inquirer.prompt(wholeTeam);
-    const managerAs = await inquirer.prompt(manager);
-    person = new Manager(allMemberAs.name, allMemberAs.id, allMemberAs.email, managerAs.officeNum);
-    employees.push(person);
-    console.log(employees)
-    otherMembers();
+	console.log('Manager details:');
+	const allMemberAs = await inquirer.prompt(wholeTeam);
+	const managerAs = await inquirer.prompt(manager);
+	person = new Manager(allMemberAs.name, allMemberAs.id, allMemberAs.email, managerAs.officeNum);
+	employees.push(person);
+	console.log(employees);
+	otherMembers();
 }
-init ();
+init();
 
+//  fucntion that will will prompt either the Engineer questions or the Intern questions 
 
 async function otherMembers() {
-    const chooseMem = await inquirer.prompt(role);
-    if (chooseMem.teamMember === "Engineer") {
-        const allEngineerA  = await inquirer.prompt(engineerQ);
-        const generalEngQs = await inquirer.prompt(wholeTeam);
-        person = new Engineer(generalEngQs.name, generalEngQs.id, generalEngQs.email, allEngineerA.githubName)
-        employees.push(person);
-        otherMembers();
-    } else if (chooseMem.teamMember === "Intern") {
-        const allInternA  = await inquirer.prompt(intern);
-        const generalInQs = await inquirer.prompt(wholeTeam);
-        person = new Intern(generalInQs.name, generalInQs.id, generalInQs.email, allInternA.school)
-        employees.push(person);
-        otherMembers();
-
-    } else if (chooseMem.teamMember ==="I dont want to add another employee!") {
-        fs.writeFile(outputPath, render(employees), function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Congrats, the file is saved!")
-        })
-        return;
-    }
-
+	const chooseMem = await inquirer.prompt(role);
+	if (chooseMem.teamMember === 'Engineer') {
+		const allEngineerA = await inquirer.prompt(engineerQ);
+		const generalEngQs = await inquirer.prompt(wholeTeam);
+		person = new  Engineer(generalEngQs.name, generalEngQs.id, generalEngQs.email, allEngineerA.githubName);
+		employees.push(person);
+		otherMembers();
+	} else if (chooseMem.teamMember === 'Intern') {
+		const allInternA = await inquirer.prompt(intern);
+		const generalInQs = await inquirer.prompt(wholeTeam);
+		person = new Intern(generalInQs.name, generalInQs.id, generalInQs.email, allInternA.school);
+		employees.push(person);
+		otherMembers();
+	} else if (chooseMem.teamMember === 'I dont want to add another employee!') {
+		fs.writeFile(outputPath, render(employees), function(err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log('File saved Sucsefully');
+		});
+		return;
+	}
 }
 
 // Write code to use inquirer to gather information about the development team members,
