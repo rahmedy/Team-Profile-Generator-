e2eee70dc710ea376b9b403bdfe6e47c120bf13d
Rayan Ahmedy <rayanahmedy@Rayans-MBP.attlocal.net>
2020-08-30 00:34:14 -0400

    WIP on the file path

--- Team profile /Develop/app.js
+++ Team profile /Develop/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
-const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const outputPath = path.join(OUTPUT_DIR, 'main.html');
 
 const render = require('./lib/htmlRenderer');
 
