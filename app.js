58992c88eec695cc2d16647ab9065658450f640e
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-09-08 18:12:00 -0400

    final bugs resolved

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const Intern = require('./lib/Intern');
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+
+
 const render = require('./lib/htmlRenderer');
 
 
-const wholeTeam = [
-	{
+const wholeTeam = [{
 		type: 'input',
 		message: 'What is the team members name?',
 		name: 'name'
@@ -35,38 +36,30 @@ const wholeTeam = [
 	}
 ];
 
-const manager = [
-	{
-		type: 'input',
-		message: 'What is your office number?',
-		name: 'officeNum'
-	}
-];
+const manager = [{
+	type: 'input',
+	message: 'What is your office number?',
+	name: 'officeNum'
+}];
 
-const engineerQ = [
-	{
-		type: 'input',
-		message: 'What is the team members github name?',
-		name: 'githubName'
-	}
-];
+const engineerQ = [{
+	type: 'input',
+	message: 'What is the team members github name?',
+	name: 'githubName'
+}];
 
-const intern = [
-	{
-		type: 'input',
-		message: "What is the intern's school name?",
-		name: 'school'
-	}
-];
+const intern = [{
+	type: 'input',
+	message: "What is the intern's school name?",
+	name: 'school'
+}];
 
-const role = [
-	{
-		type: 'list',
-		message: 'What type of team member would you like to add?',
-		name: 'teamMember',
-		choices: [ 'Engineer', 'Intern', 'I dont want to add another employee!' ]
-	}
-];
+const role = [{
+	type: 'list',
+	message: 'What type of team member would you like to add?',
+	name: 'teamMember',
+	choices: ['Engineer', 'Intern', 'I dont want to add another employee!']
+}];
 
 // variables that will be changed
 let employees = [];
@@ -103,14 +96,16 @@ async function otherMembers() {
 		employees.push(person);
 		console.log("hello")
 		otherMembers();
-	} else if (chooseMem.teamMember ==="I dont want to add another employee!") {
-        fs.writeFile(outputPath, render(employees), function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Congrats, the file is saved!")
-        })
-        return;
+	} else if (chooseMem.teamMember === "I dont want to add another employee!") {
+		fs.writeFile(outputPath, render(employees), function (err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(OUTPUT_DIR);
+			console.log(outputPath);
+			console.log(" The file is saved!")
+		})
+		return;
 	}
 };
 // }(chooseMem.teamMember === 'I dont want to add another employee') 
