51d2a4d28984d1d33a34b03b9b1ca30f5f766511
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-09-09 21:32:16 -0400

    fixed bugs final push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
-// const jest = require('jest');
+
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -77,7 +77,7 @@ async function init() {
 }
 init();
 
-//  fucntion that will will prompt either the Engineer questions or the Intern questions
+//  fucntion that will will prompt either the engineer or the intern questions
 
 async function otherMembers() {
 	// console.log("hellopw")
@@ -107,28 +107,3 @@ async function otherMembers() {
 		return;
 	}
 };
-// }(chooseMem.teamMember === 'I dont want to add another employee') 
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
